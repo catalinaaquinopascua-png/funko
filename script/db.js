5ab5b508d10bded930864ca401ff1debac8ed42e
DanielArg100 <danicaffe100@gmail.com>
2026-05-13 06:59:53 -0300

    CambiosEnJSyAlgunosEnHTML

--- script/db.js
+++ script/db.js
@@ -3,7 +3,7 @@ export const ListaProductos = [{
     descripción: "El icónico asesino silencioso llega en versión Funko Pop con su clásica máscara blanca y cuchillo en mano. Una figura imprescindible para los amantes del terror slasher y las películas de culto.",
     precio: 95000,
     stock: 2,
-    img: "../src/FunkoPops/ACFunkoPopTravella.png"
+    img: "../src/FunkoPops/ACFunkoPopFederico.png"
 }, {
     nombre: "Pablo Gareis - Freddy Krueger",
     descripción: "Directamente desde las pesadillas, Freddy aparece con su sombrero desgastado, suéter rojo y verde, y su aterrador guante de cuchillas. Perfecto para una colección de horror clásico.",
@@ -15,7 +15,7 @@ export const ListaProductos = [{
     descripción: "El legendario asesino de Crystal Lake cobra vida en este Funko Pop lleno de detalles, incluyendo su famosa máscara de hockey y apariencia intimidante.",
     precio: 95200,
     stock: 4,
-    img: "../src/FunkoPops/ACFunkoPopBruno.png"
+    img: "../src/FunkoPops/AcFunkoPopBrunoJason.png"
 }, {
     nombre: "Gutierrez Sol - Ghostface",
     descripción: "El misterioso asesino de la saga Scream llega con su túnica negra y máscara fantasmal. Un personaje imprescindible para fanáticos del suspense y el terror moderno.",
@@ -27,65 +27,65 @@ export const ListaProductos = [{
     descripción: "El terrorífico payaso creado por Stephen King aparece con su sonrisa perturbadora y globo rojo característico. Ideal para darle un toque espeluznante a cualquier colección.",
     precio: 89100,
     stock: 6,
-    img: "../src/FunkoPops/ACFunkoPopDaniel.png"
+    img: "../src/FunkoPops/ACFunkoPopDanielPenny.png"
 }, {
     nombre: "Joaquin Paz - Chucky",
     descripción: "El muñeco asesino más famoso del cine llega en una versión Funko Pop adorable y aterradora al mismo tiempo, con su overol clásico y mirada siniestra.",
     precio: 90670,
     stock: 3,
-    img: "../src/FunkoPops/ACFunkoPopNemesis.png"
+    img: "../src/FunkoPops/ACFunkoPopChucky.png"
 }, {
     nombre: "Elba Aquino - Leatherface",
     descripción: "Con su motosierra y máscara hecha de piel humana, Leatherface representa uno de los villanos más brutales e icónicos del cine de horror.",
     precio: 98710,
     stock: 6,
-    img: "../src/FunkoPops/ACFunkoPopClaireRedfield.png"
+    img: "../src/FunkoPops/ACFunkoPopLeatherface.png"
 }, {
     nombre: "Martina Borda - Jack Skellington",
     descripción: "El Rey Calabaza de Halloween Town destaca con su elegante traje negro y sonrisa inolvidable. Una figura imprescindible para coleccionistas Disney y amantes de Halloween.",
     precio: 89945,
     stock: 4,
-    img: "../src/FunkoPops/ACFunkoPopRebecca.png"
+    img: "../src/FunkoPops/ACFunkoPopJack.png"
 }, {
     nombre: "Elias Paniagua - Beetlejuice",
     descripción: "El excéntrico fantasma de traje a rayas llega con toda su energía caótica y humor oscuro. Un Funko Pop perfecto para fans del terror cómico y Tim Burton.",
     precio: 76050,
     stock: 1,
-    img: "../src/FunkoPops/ACFunkoPopBuhonero.png"
+    img: "../src/FunkoPops/ACFunkoPopBeetlejuice.png"
 }, {
     nombre: "Gabriel - Sam (Trick ’r Treat)",
     descripción: "El pequeño guardián de Halloween aparece con su pijama naranja y saco de tela en la cabeza. Un personaje de culto para verdaderos fanáticos de la festividad.",
     precio: 59900,
     stock: 0,
-    img: "../src/FunkoPops/ACFunkoPopHunk.png"
+    img: "../src/FunkoPops/ACFunkoPopSam.png"
 }, {
     nombre: "Franco - Vecna",
     descripción: "El aterrador villano del Upside Down llega con un diseño lleno de detalles oscuros y una apariencia monstruosa que impacta en cualquier colección.",
     precio: 89350,
     stock: 2,
-    img: "../src/FunkoPops/ACFunkoPopChris.png"
+    img: "../src/FunkoPops/ACFunkoPopVecna.png"
 }, {
     nombre: "Jazmin Ayestaran - Wednesday Addams",
     descripción: "La hija más famosa de la Familia Addams aparece con su uniforme oscuro y expresión seria. Perfecta para fans del misterio y la estética gótica.",
     precio: 98100,
     stock: 5,
-    img: "../src/FunkoPops/ACFunkoPopSheva.png"
+    img: "../src/FunkoPops/ACFunkoPopMerlina.png"
 }, {
     nombre: "Ludmila - Pinhead",
     descripción: "¡Funko tiene tales visiones para mostrarte! Añade al líder de los Cenobitas a tu colección de terror con este Funko Pop! de Pinhead.",
     precio: 76520,
     stock: 3,
-    img: "../src/FunkoPops/ACFunkoPopAlexia.png"
+    img: "../src/FunkoPops/ACFunkoPopPinhead.png"
 }, {
     nombre: "Gabi - Nemesis",
     descripción: "El arma biológica más temida de Resident Evil llega en una versión Funko Pop intimidante y detallada, ideal para fanáticos de los videojuegos de horror.",
     precio: 98550,
     stock: 7,
-    img: "../../src/FunkoPops/ACFunkoPopT103.png"
+    img: "../src/FunkoPops/AcFunkoPopNemesis.png"
 }, {
     nombre: "Lucas Billar - Billy the Puppet",
     descripción: "El macabro títere de Saw llega con su traje elegante y mejillas en espiral. Una pieza esencial para coleccionistas del terror psicológico y películas de culto.",
     precio: 89620,
     stock: 2,
-    img: "../src/FunkoPops/ACFunkoPopSergei.png"
+    img: "../src/FunkoPops/ACFunkoPopBilly.png"
 }]
